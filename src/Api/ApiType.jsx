53cbc6c84d976b827b2d6f398b3604d7b435788b
Nischit007 <nischitpantha@gmail.com>
2025-05-19 14:10:09 +0545

    docker containerized

--- src/Api/ApiType.jsx
+++ src/Api/ApiType.jsx
@@ -1,8 +1,12 @@
-export const api = "http://5.189.139.40:8080";
+// export const api = "http://5.189.139.40:8080";
 
-export const oauth = "http://localhost:8080";
-// for share
+export const api = 'https://backend.studiofreedom.org';
+
+
+// export const api = "https://www.studiofreedom.org";
+
+export const oauth = 'https://backend.studiofreedom.org';
 
 export const frontendUrl="http://localhost:5173"
-
+// for share
 
