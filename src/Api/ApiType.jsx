d5f4b7431d23e1ac3e8d01bd4bb6d60e0ca699ac
Nischit007 <nischitpantha@gmail.com>
2025-09-06 06:43:52 +0545

    pull code of updated brnahc

--- src/Api/ApiType.jsx
+++ src/Api/ApiType.jsx
@@ -1,12 +1,12 @@
-// export const api = "http://5.189.139.40:8080";
-
-// export const api = 'https://backend.studiofreedom.org';
 
 
-export const api = 'http://localhost:8080';
+
+export const api = 'https://backend.studiofreedom.org';
+
+
 
 export const oauth = 'https://backend.studiofreedom.org';
 
 export const frontendUrl="https://studiofreedom.org"
-// for share
+
 
